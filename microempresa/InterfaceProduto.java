6bd3603cb16c4bce3f49296351c186f931cb1117
Naylane <naylaneribeiro4@gmail.com>
2021-08-26 21:11:57 -0300

    Criando InterfaceProduto

---
+++ microempresa/InterfaceProduto.java
@@ -0,0 +1,6 @@
+package microempresa;
+
+public class InterfaceProduto {
+	
+	
+}
--- microempresa/MenuPrincipal.java
+++ microempresa/MenuPrincipal.java
@@ -1,19 +1,54 @@
 package microempresa;
 
+import java.util.Scanner;
+
 public class MenuPrincipal {
 
-	private RepositorioDeCliente repositorioDeCliente;
-	private RepositorioDeProduto repositorioDeProduto;
-	private RepositorioDeVenda repositorioDeVenda;
+	private InterfaceCliente interfaceCliente;
+	private InterfaceProduto interfaceProduto;
+	private InterfaceVenda interfaceVenda;
+	private Scanner ler;
+	
 	
 	public MenuPrincipal() {
-		RepositorioDeCliente repositorioDeCliente;
-		RepositorioDeProduto repositorioDeProduto;
-		RepositorioDeVenda repositorioDeVenda;
+		this.ler = new Scanner(System.in);
+		InterfaceCliente interfaceCliente;
+		InterfaceProduto interfaceProduto;
+		InterfaceVenda interfaceVenda;
 	}
+
 	
 	public void iniciarGerenciamento(){
-		System.out.println("");
+
+		int opcao;
+		
+		do {
+			System.out.println("---- Menu Principal ----");
+			System.out.println("Essas são as opçoes de gerenciamento do menu:");
+			System.out.println("1- Clientes");
+			System.out.println("2- Produtos");
+			System.out.println("3- Vendas");
+			System.out.println("0- Sair");
+			System.out.println("Selecione a opção desejada:");
+			opcao = ler.nextInt();
+			
+			switch(opcao) {
+				case 1: 
+			
+					break;
+				case 2:
+			
+					break;
+				case 3:
+			
+					break;
+			}
+		
+		}while (opcao!=0);
+		
+		
+		
+		
 	}
 
 }
